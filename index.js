35b9731f973915df400960e09292632e5b40fcc0
Priya434 <92980244+Priya434@users.noreply.github.com>
2022-04-23 19:58:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+document.getElementById("nav-bar-links-2").classList.remove("nav-bar-links-2");
+
 function myFunction() {
   var x = document.getElementById("nav-bar-links-2");
   x.classList.toggle("nav-bar-links-2");
